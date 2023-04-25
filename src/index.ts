1c5bd501f6d12ea67d8c907be411073573e6de73
Alexander Ciesielski <alexander@ciesielski.co>
2023-04-25 19:27:28 +0200

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export const blabla = '';
+console.log('lala');
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
+
+const prodConfig = require('./webpack.prod');
+const devConfig = require('./webpack.dev');
+
+const resolveApp = (relativePath) => path.resolve(__dirname, relativePath);
+
+const getPublicPath = () => {
+  const homePage = require(resolveApp('package.json')).homepage;
+
+  if (process.env.NODE_ENV === 'development') {
+    return '';
+  } else if (process.env.PUBLIC_URL) {
+    return process.env.PUBLIC_URL;
+  } else if (homePage) {
+    return homePage;
+  }
+  return '/';
+};
+
+const getEnvVariables = () => ({
+  PUBLIC_URL: getPublicPath(),
+  VERSION: require(resolveApp('package.json')).version,
+});
+
+module.exports = function () {
+  const isEnvProduction = process.env.NODE_ENV === 'production';
+
+  const commonConfig = {
+    entry: './src/index.ts',
+    output: {
+      filename: '[name].bundle.js',
+      clean: true,
+    },
+    plugins: [new webpack.ProgressPlugin(), new CleanWebpackPlugin()],
+
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          loader: 'swc-loader',
+          include: [resolveApp('src')],
+          exclude: [/node_modules/],
+        },
+      ],
+    },
+
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+  };
+
+  if (isEnvProduction) return merge(commonConfig, prodConfig);
+  else return merge(commonConfig, devConfig);
+};
